1cad54c0aa04e6f511e2fee64f5cc4d3e9a4205e
Kamosoharden <145597958+Kamosoharden@users.noreply.github.com>
2024-05-28 16:52:03 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+</head>
+<style>
+    body{
+        display: flex;
+        width: 100%;
+        height: 100vh;
+    }
+    .all{
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        margin: auto;
+    }
+    .all > *{
+        margin: auto;
+    }
+</style>
+<body>
+    <div class="all">
+        <h1>WELCOME TO COM AGENCY</h1><br>
+        <P>We speak Digital, fluently</P>
+    </div>
+</body>
+</html>
